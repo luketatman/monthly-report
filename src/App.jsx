89eac0c53b2447621698698f35854d9dbbb3bdfb
luketatman <luketatman16@gmail.com>
2026-04-07 10:26:04 -0400

    Fix GitHub Pages routing: add basename to BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ function App() {
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClientInstance}>
-        <Router>
+        <Router basename="/monthly-report">
           <NavigationTracker />
           <AuthenticatedApp />
         </Router>
