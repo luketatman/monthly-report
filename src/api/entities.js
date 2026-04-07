83415897e203bfe8647772f6ed3837af18e7397c
luketatman <luketatman16@gmail.com>
2026-04-07 11:38:22 -0400

    Fix build: add all entity exports to src/api/entities.js and fix broken @/entities/* import paths

--- src/api/entities.js
+++ src/api/entities.js
@@ -2,7 +2,14 @@ import { base44 } from './base44Client';
 
 
 export const Query = base44.entities.Query;
-
+export const FinancialData = base44.entities.FinancialData;
+export const Region = base44.entities.Region;
+export const MonthlySubmission = base44.entities.MonthlySubmission;
+export const OfficeSubmission = base44.entities.OfficeSubmission;
+export const WinLoss = base44.entities.WinLoss;
+export const Pitch = base44.entities.Pitch;
+export const PersonnelUpdate = base44.entities.PersonnelUpdate;
+export const ServiceLineSubmission = base44.entities.ServiceLineSubmission;
 
 
 // auth sdk:
