b3777470ed9ddfbbc5b0a796e8a10d2c6f9d01bc
luketatman <luketatman16@gmail.com>
2026-05-14 11:36:48 -0400

    Add src/entities shim files to fix build imports

---
+++ src/entities/all.js
@@ -0,0 +1,2 @@
+export { Query, FinancialData, Region, MonthlySubmission, OfficeSubmission, WinLoss, Pitch, PersonnelUpdate, ServiceLineSubmission, RegionPin } from '@/api/entities';
+export { User } from '@/api/entities';
